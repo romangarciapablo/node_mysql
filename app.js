e2d2813d29cbe8ef4884a2a1eb5018247647e9a8
Pablo Román <pablo.roman@delonia.com>
2021-07-21 13:42:08 +0200

    Corrección para no tener problemas con CORS

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const mysql = require('mysql');
 
-const bodyParser = require('body-parser');
-
 const PORT = process.env.PORT || 3050;
 
 const app = express();
 
+//Middleware para no tener problemas con CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.urlencoded({extended: true}))
 
 // mysql
 const connection = mysql.createConnection({
